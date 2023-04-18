22458d8862c1ddd89900e614a34fae5cff3178ff
IT20660116 <79563126+IT20660116@users.noreply.github.com>
2023-04-18 13:39:11 +0000

    newly Added

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+  return render_template('Home.html')
 
 
 app.run(host='0.0.0.0', port=81)
+ 
